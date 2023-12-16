0507a57c717ff4d4609da60d11ab29fdbe49a6bb
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-16 18:08:45 +0100

    - ADD function for random number - ADD cycle for generate 5 different number - ADD array where push random number - ADD prompt for ask user five number - ADD array where push user number - ADD array where push only right numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,63 @@
-// Test funzionamento
-console.log('Template');
+
+const numberMemory = [];
+
+for (let i = 0; i < 5; i++){
+
+    let trueNumber = randomNumber(1, 50);
+
+
+    let foundInArray = numberMemory.includes(trueNumber); 
+
+    while(foundInArray == true){
+
+        trueNumber = randomNumber(1, 50);
+        foundInArray = numberMemory.includes(trueNumber); 
+
+    }
+
+    numberMemory.push(trueNumber); 
+    console.log(numberMemory)
+    
+}
+
+setTimeout(() => {
+    document.getElementById('number-memory').innerHTML = numberMemory;  
+}, 1000);
+
+setTimeout(() => {
+    document.getElementById('number-memory').innerHTML = '';  
+}, 30000);
+
+const numberUser = [];
+const numberVerified = [];
+
+setTimeout(() => {
+    for(let j = 0; j < 5; j++){
+        const userNumber = parseInt(prompt('Inserisci il numero'))
+        numberUser.push(userNumber);
+        
+
+        if(numberMemory.includes(userNumber)){
+            numberVerified.push(userNumber);
+            document.getElementById('result').innerHTML = 'Hai indovinato i numeri '+ numberVerified;
+
+        }
+        
+        console.log(numberVerified);
+
+    }
+}, 32000);
+
+
+
+
+
+
+
+
+
+//----- FUNCTIONS ----------------------------------------------
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
